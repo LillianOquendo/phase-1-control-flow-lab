7f362bdeeeb37a379b2b9a6a9f0653da907b888a
Lillian Oquendo <LillianOquendo1@gmail.com>
2023-03-18 19:31:44 -0400

    Control Flow Lab

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let x = 56;
+function scuberGreetingForFeet(x){
+if (x <= 400){
+return ('This one is on me!');
 }
-
-function ternaryCheckCity(){
-  // Write your code here!
+else if (x > 400 && x <= 2000){
+return ('That will be twenty bucks.');
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+else if (x > 2000 && x < 2499){
+  return ('I will gladly take your thirty bucks.');
+}
+else if (x >= 2500){
+return ('No can do.');
+}
+}
+let y = ('Chicago')
+function ternaryCheckCity(y){
+ return y === 'NYC'? 'Ok, sounds good.': `No go.`
+}
+let z = ('')
+function switchOnCharmFromTip(z){
+  switch (z){
+  case 'generous':
+    return 'Thank you so much.'
+  case 'not as generous':
+    return 'Thank you.'
+  default:
+    return 'Bye.'
+}
 }
